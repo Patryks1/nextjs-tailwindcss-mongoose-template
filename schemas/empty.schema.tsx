e1f141ea2681cd8cd58e2380fb84d0f7ef413f9b
Patryk Slowinski <slowinskipatryk1321@gmail.com>
2021-05-28 23:33:21 +0100

    Typed goose

--- schemas/empty.schema.tsx
+++ schemas/empty.schema.tsx
@@ -1,5 +1,25 @@
-import mongoose from 'mongoose';
+import mongoose, { Document, Model } from 'mongoose';
 
-const schema = new mongoose.Schema({});
+export interface IEmptyDocument extends Document {
+  key: string;
+  expired: Date;
+  created: Date;
+}
 
-export default mongoose.models.Schema || mongoose.model('Schema', schema);
+const schema = new mongoose.Schema({
+  key: {
+    type: String
+  },
+  expired: {
+    type: Date
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const model =
+  (mongoose.models.schema as Model<IEmptyDocument>) ||
+  mongoose.model<IEmptyDocument>('schema', schema);
+export default model;
